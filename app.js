025e31c7362af72b9bb36309a441ceeaaf9d975e
vivek-karma <karma.vishwakarma@gmail.com>
2019-09-22 16:47:42 +0530

    first commit

--- app.js
+++ app.js
@@ -11,6 +11,11 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
+if(process.env.NODE_ENV === 'production') {
+  app.get('/*', function (req, res) {
+   	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
